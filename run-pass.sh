558ad08866893b1e269aa73882ffc67cd771abdb
Scott Carr <carr27@purdue.edu>
2014-09-23 16:44:46 -0400

    add more options

--- run-pass.sh
+++ run-pass.sh
@@ -1,5 +1,8 @@
 BASE=~/data-integrity/build
 LIB=$BASE/lib/LLVMDataIntegrity.so
 OPT=$BASE/bin/opt
-$OPT -load $LIB -DataIntegrityPass < test-fcfi.ll > /dev/null
+#$OPT -default-data-layout="" -targetlibinfo -basicaa -datalayout -load $LIB -DataIntegrityPass < test.ll > /dev/null
+#$OPT -targetlibinfo -basicaa -datalayout -load $LIB -DataIntegrityPass < test.ll > /dev/null
+$OPT -load $LIB -DataIntegrityPass < test.ll > /dev/null
+#$OPT -load $LIB -DataIntegrityPass < test-fcfi.ll > /dev/null
 #$OPT -load $LIB -DataIntegrityPass < test.ll
