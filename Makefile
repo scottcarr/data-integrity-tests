252c50264d07999d08bed17dc0a961faa05ccba8
Scott Carr <carr27@purdue.edu>
2014-10-02 12:48:39 -0400

    add more tests and generate both versions of bc

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
 #CXX=~/sunblaze-ucb/build/bin/clang++
 CXX=~/data-integrity/build/bin/clang++
-SRC=test8.cpp
+SRC=test9.cpp
 
 all: $(SRC)
 	$(CXX) -flto -fcfi=fptr-full  $(SRC)
 	$(CXX) -S -emit-llvm  $(SRC) -o test.ll
+	$(CXX) $(SRC) -o normal
+	$(CXX) -S -emit-llvm  $(SRC) -o normal.ll
 
 clean:
 	rm a.out *.ll
---
+++ test9.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef struct {
+  int val;
+} Key;
+
+
+__attribute__((annotate("sensitive"))) Key mykey;
+//Key *keyPtr;
+Key *keys;
+
+void foo() {
+  cout << "keys address is: " << keys << endl;
+  for (int i = 0; i < 3; i++) {
+    //cout << (*keys)[i].val << endl; 
+    int k = keys[i].val;
+    cout << k << endl; 
+  }
+}
+
+int main() {
+  Key *k = (Key*)malloc(sizeof(Key)*3);
+  cout << "k's address is: " << k << endl;
+  for (int i = 0; i < 3; i++) {
+    int j;
+    cin >> j;
+    k[i].val = j;
+  }
+  keys = k;
+  foo();
+  return 0;
+}
