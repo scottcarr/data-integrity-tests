9b32c125336cb63205e1b7f69c93cdac96865717
Scott Carr <carr27@purdue.edu>
2014-10-20 12:57:19 -0400

    add test12

---
+++ test12.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef struct {
+  long int val;
+} Key;
+
+const int N = 3;
+
+__attribute__((annotate("sensitive"))) Key mykey;
+Key *ptr;
+
+int main() {
+  ptr = (Key*)malloc(sizeof(Key)*N);
+  int j;
+  for(int i = 0; i < N+1; ++i) {
+    cin >> j;
+    ptr[i].val = j;
+  }
+  cout << "should never get here\n";
+  return 0;
+}
