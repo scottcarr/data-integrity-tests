c074159dc4a47098b8bf483b5c1af51d33ea8bae
javery <javeryk21@gmail.com>
2014-11-11 02:03:28 -0800

    added more organization to secutiry tests

---
+++ security_tests/code/sec_test6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdlib.h>
+#include "../../safe_functions.h"
+
+
+using namespace std;
+
+/*
+overwrite boundary info in protected memory
+*/
+
+typedef struct {
+  long int val;
+} Key;
+
+const int N = 3;
+
+__attribute__((annotate("sensitive"))) Key mykey;
+//Key *ptr;
+
+int main() {
+  Key addr;
+  Key *addr1 = (Key*)malloc(sizeof(Key));
+  addr.val = 5;
+  addr1 = (Key*)&addr + sizeof(long int);
+  addr1->val = 18;	
+
+
+
+  return 0;
+}
