c21468b4a35d22e4a3246eb910aecdc54c9d1daf
javery <javeryk21@gmail.com>
2014-12-08 08:25:59 -0800

    modified to run-tests

--- security_tests/run-tests.py
+++ security_tests/run-tests.py
@@ -11,12 +11,93 @@ def die(test, expected, actual):
 
 
 # sec_test
-in_data = "42\n9999\n1234\n4321\n555\n"
-expected = "42\n9999\n1234\n4321\n555\n"
+print "Security test 1"
 p = Popen(["./sec_test"], stdout=PIPE, stdin=PIPE)
-stdout_dat = p.communicate(input=in_data)[0]
+stdout_dat = p.communicate(input=None)[0]
+print stdout_dat
 
-if stdout_dat != expected:
-    die("sec_test", expected, stdout_dat)
-else:
-    print "sec_test passed."
+print "\n"
+#if stdout_dat != expected:
+#    die("sec_test", expected, stdout_dat)
+#else:
+#    print "sec_test passed."
+
+
+# sec_test1
+print "Security test 2"
+p = Popen(["./sec_test1"], stdout=PIPE, stdin=PIPE)
+stdout_dat = p.communicate(input=None)[0]
+print stdout_dat
+print "\n"
+#if stdout_dat != expected:
+
+#    die("sec_test", expected, stdout_dat)
+#else:
+#    print "sec_test passed."
+
+# sec_test2
+print "Security test 3"
+p = Popen(["./sec_test2"], stdout=PIPE, stdin=PIPE)
+stdout_dat = p.communicate(input=None)[0]
+print stdout_dat
+print "\n"
+#if stdout_dat != expected:
+
+#if stdout_dat != expected:
+#    die("sec_test", expected, stdout_dat)
+#else:
+#    print "sec_test passed."
+
+# sec_test3
+print "Security test 4"
+p = Popen(["./sec_test3"], stdout=PIPE, stdin=PIPE)
+stdout_dat = p.communicate(input=None)[0]
+print stdout_dat
+print "\n"
+#if stdout_dat != expected:
+
+#if stdout_dat != expected:
+#    die("sec_test", expected, stdout_dat)
+#else:
+#    print "sec_test passed."
+
+# sec_test4
+print "Security test 5"
+p = Popen(["./sec_test4"], stdout=PIPE, stdin=PIPE)
+stdout_dat = p.communicate(input=None)[0]
+print stdout_dat
+print "\n"
+#if stdout_dat != expected:
+
+#if stdout_dat != expected:
+#    die("sec_test", expected, stdout_dat)
+#else:
+#    print "sec_test passed."
+
+# sec_test5
+print "Security test 6"
+p = Popen(["./sec_test5"], stdout=PIPE, stdin=PIPE)
+stdout_dat = p.communicate(input=None)[0]
+print stdout_dat
+print "\n"
+#if stdout_dat != expected:
+
+#if stdout_dat != expected:
+#    die("sec_test", expected, stdout_dat)
+#else:
+#    print "sec_test passed."
+
+# sec_test6
+print "Security test 7"
+p = Popen(["./sec_test6"], stdout=PIPE, stdin=PIPE)
+stdout_dat = p.communicate(input=None)[0]
+print stdout_dat
+print "\n"
+
+#if stdout_dat != expected:
+
+#if stdout_dat != expected:
+#    die("sec_test", expected, stdout_dat)
+#else:
+#    print "sec_test passed."
+
