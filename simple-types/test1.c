42d30cad19441af8904f34bda68083e3d492ad19
Scott Carr <carr27@purdue.edu>
2015-09-17 18:09:49 -0400

    make test more complicated

--- simple-types/test1.c
+++ simple-types/test1.c
@@ -1,12 +1,32 @@
 #include <stdlib.h>
+
+struct object {
+  struct vector3 *position, *velocity;
+  int* mass;
+};
+
 struct vector3 {
   int x, y, z;
 };
 
-__attribute__((annotate("sensitive"))) struct vector3 x;
+__attribute__((annotate("sensitive"))) struct vector3 *x;
+
+void foo(struct vector3* x) {
+  printf("can it inline printf?\n");
+  x->z = x->x;
+}
 
 int main(int argc, char** argv) {
-  struct vector3* ptr;
-  ptr = malloc(sizeof(struct vector3)*10);
+  struct vector3* safe_ptr;
+  int* unsafe_ptr;
+  safe_ptr = malloc(sizeof(struct vector3)*10);
+  unsafe_ptr = malloc(30);
+  //safe_ptr = unsafe_ptr;
+  struct object obj;
+  obj.position = safe_ptr;
+  obj.velocity = safe_ptr + 1;
+  //obj.mass = safe_ptr + 2;
+  foo(safe_ptr);
+  foo(obj.mass);
   return 0;
 }
